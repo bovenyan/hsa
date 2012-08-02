8b8d4e6e77fa1bc0c4cc9818e98d065a9939f721
Peyman <kazemian@stanford.edu>
2012-08-01 23:12:59 -0700

    adding template for tf gen and reachability test

--- hsa-python/config_parser/graph_xml_parser.py
+++ hsa-python/config_parser/graph_xml_parser.py
@@ -18,7 +18,15 @@ class graph_xml(object):
     self.links = {}
     self.nodes = {}
     self.port_name_translator = cisco_router.get_ethernet_port_name
+    self.device_types = []
     pass
+  
+  def set_device_types(self,types):
+    '''
+    types is a list of device types to be used in topology. (e.g. ["Router"])
+    setting this to an empty list [] will match on any device type.
+    '''
+    self.device_types = types
   
   def _create_elem_tree(self,filename):
     f = open(filename,'r')
@@ -35,7 +43,9 @@ class graph_xml(object):
     for node in nodes:
       node_id = node.attrib["id"]
       node_name = node.find("name").text
-      self.nodes[node_id] = node_name
+      node_type = node.find("deviceType").text
+      if (len(self.device_types) == 0 or node_type in self.device_types):
+        self.nodes[node_id] = node_name
 
   def read_graphs_xml(self,filename):
     tree = self._create_elem_tree(filename)
@@ -70,8 +80,15 @@ class graph_xml(object):
     result = []
     for edge_id in self.edges.keys():
       (src_id,dst_id) = self.edges[edge_id]
-      src_name = self.nodes[src_id]
-      dst_name = self.nodes[dst_id]
-      (src_port,dst_port) = self.links[edge_id]
-      result.append((src_name,src_port,dst_name,dst_port))
+      if (src_id in self.nodes.keys() and dst_id in self.nodes.keys()):
+        src_name = self.nodes[src_id]
+        dst_name = self.nodes[dst_id]
+        (src_port,dst_port) = self.links[edge_id]
+        result.append((src_name,src_port,dst_name,dst_port))
     return result
+  
+  def generate_node_names(self):
+    results = []
+    for node_id in self.nodes.keys():
+      results.append(self.nodes[node_id])
+    return results
