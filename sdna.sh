8997c6646c330f01b146b6677e1a8c335b19bf63
Peyman Kazemian <kazemian@stanford.edu>
2013-06-20 19:57:16 -0700

    sdna script

--- sdna.sh
+++ sdna.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+export PYTHONPATH=$PYTHONPATH:~/hassel-public/hsa-python
 cd hsa-python
 ./setup.sh
-cd ../hassel-c/
+cd examples/stanford
+mkdir tf_stanford_backbone
+python generate_stanford_backbone_tf.py
+cd ../../../hassel-c/
 make
-./gen stanford
+./clean.sh
